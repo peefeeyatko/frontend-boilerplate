af3ac99f3d04dcf0894a1489b1878d1b377d09b7
Marshall Sutton <marshall.anthonys@gmail.com>
2016-05-15 18:41:26 +1000

    define gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+/**
+* @desc
+* This is a Gulpfile for task running.
+* For development tasks run command 'gulp'
+* For staging or production builds run 'gulp --build'
+*
+* Include all required plugins at the top of this file.
+*/
+
+var gulp      = require('gulp'),
+    sass      = require('gulp-sass'),
+    argv      = require('yargs').argv,
+    concat    = require('gulp-concat'),
+    jshint    = require('gulp-jshint'),
+    uglify    = require('gulp-uglify'),
+    strip     = require('gulp-strip-debug');
+
+
+// Define the paths to the project's assets
+var config = {
+    js: {
+        in: 'public/js/src/**/*.js',
+        out: 'public/js/dist/'
+    },
+
+    sass: {
+        in: 'public/css/src/**/*.scss',
+        out: 'public/css/dist/'
+    }
+};
+
+// Define Gulp tasks
+gulp.task('sass', function() {
+    return gulp.src(config.sass.in)
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(concat('style.min.css'))
+      .pipe(gulp.dest(config.sass.out));
+});
+
+gulp.task('js', function() {
+	return gulp.src(config.js.in)
+      .pipe(uglify())
+      .pipe(concat('main.min.js'))
+      .pipe(gulp.dest(config.js.out));
+});
+
+gulp.task('lint', function() {
+	return gulp.src(config.js.in)
+	  .pipe(jshint())
+	  .pipe(jshint.reporter('default', {verbose: true}));
+});
+
+gulp.task('default', ['sass', 'js'], function() {
+    // Watch js for changes
+	gulp.watch(config.js.in, function() {
+        gulp.start('lint', 'js');
+	});
+
+	// Watch sass for changes
+	gulp.watch(config.sass.in, function() {
+		gulp.start('sass');
+	});
+});
