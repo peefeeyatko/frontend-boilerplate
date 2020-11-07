43d640ef5829faaa00babb488ebe14f546d5072f
peefeeyatko <marshall.anthonys@gmail.com>
2020-11-07 21:04:16 +1100

    update for gulp 4

--- README.md
+++ README.md
@@ -12,25 +12,23 @@ An npm and gulp boilerplate for projects requiring task running
 * run `npm install` this will install all required plugins and dependencies defined in package.json
 * Update config object in gulpfile.js
 ```js
-// Define the in and out paths to your project's assets (sass, js)
-var config = {
+// Define the in and out paths to your project's assets
+let config = {
     js: {
         in: 'src/js/*.js',
-        out: 'public/js/'
+        out: 'dist/js'
     },
 
-    sass: {
-        in: 'src/sass/**/*.scss',
-        out: 'public/css/'
+    css: {
+        in:  'src/css/*.css',
+        out: 'dist/css'
     }
 };
 ```
 * run `gulp` while developing
-* run `gulp --build` for production ready assets
 
 ### Gulp tasks
 Short description of each gulp task defined in gulpfile.js
-* `gulp sass` - Compiles, minifies and concatenates all sass files in src directory
+* `gulp css` - Compiles, minifies and concatenates all css files in the src directory
 * `gulp js` - Minifies and concatenates all js files in src directory
 * `gulp lint` - Pipes all js files in src directory through the jshint reporter. Rules are configured in the .jshintrc file
-* `gulp build` - Strips console and debugger statements from js files before minifying and concatenation
--- gulpfile.js
+++ gulpfile.js
@@ -2,91 +2,60 @@
 * @desc
 * This is a Gulpfile for task running.
 * For development tasks run command 'gulp'
-* For staging or production builds run 'gulp --build'
 *
 * Include all required plugins at the top of this file.
 */
 
-var gulp      = require('gulp'),
-    sass      = require('gulp-sass'),
-    argv      = require('yargs').argv,
-    concat    = require('gulp-concat'),
-    jshint    = require('gulp-jshint'),
-    uglify    = require('gulp-uglify'),
-    strip     = require('gulp-strip-debug');
+const {src, dest, watch, series} = require('gulp');
 
+const argv = require('yargs').argv;
+const concat = require('gulp-concat');
+const jshint = require('gulp-jshint');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
 
 // capture arguments passed to the gulp command
 const ARGS = argv;
 
-// define the paths to the project's assets
-var config = {
+// define the paths to your project's assets
+let config = {
     js: {
         in: 'src/js/*.js',
-        out: 'public/js/'
+        out: 'dist/js'
     },
 
-    sass: {
-        in: 'src/sass/**/*.scss',
-        out: 'public/css/'
+    css: {
+        in:  'src/css/*.css',
+        out: 'dist/css'
     }
 };
 
 // define gulp tasks
-gulp.task('sass', function() {
-    return gulp.src([
-        'node_modules/bootstrap/dist/css/bootstrap.min.css',
-        config.sass.in
+function css() {
+    return src([
+        config.css.in
     ])
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(concat('style.min.css'))
-    .pipe(gulp.dest(config.sass.out));
-});
+    .pipe(dest(config.css.out));
+}
 
-gulp.task('js', function() {
-	return gulp.src([
-        'node_modules/jquery/dist/jquery.min.js',
-        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+function js() {
+	return src([
         config.js.in
     ])
     .pipe(uglify())
     .pipe(concat('main.min.js'))
-    .pipe(gulp.dest(config.js.out));
-});
+    .pipe(dest(config.js.out));
+}
 
-gulp.task('lint', function() {
-	return gulp.src(config.js.in)
-	    .pipe(jshint())
-	    .pipe(jshint.reporter('default', {verbose: true}));
-});
+function lint() {
+    return src(config.js.in)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default', {verbose: true}));  
+}
 
-gulp.task('build', function() {
-    gulp.start('sass');
-    
-    // strip any debug code from JS
-    return gulp.src([
-        'node_modules/jquery/dist/jquery.min.js',
-        'node_modules/bootstrap/dist/js/bootstrap.min.js',
-        config.js.in
-    ])
-    .pipe(strip())
-    .pipe(uglify())
-    .pipe(concat('main.min.js'))
-    .pipe(gulp.dest(config.js.out));
-});
-
-gulp.task('default', ['sass', 'js'], function() {
-    if (ARGS.build === true) {
-        gulp.start('build');
-    } else {
-        // watch JS for changes
-    	gulp.watch(config.js.in, function() {
-            gulp.start('lint', 'js');
-    	});
-
-    	// watch SASS for changes
-    	gulp.watch(config.sass.in, function() {
-    		gulp.start('sass');
-    	});
-    }
-});
+exports.default = function() {
+    watch(config.js.in, {ignoreInitial: false}, series(lint, js));
+    watch(config.css.in, {ignoreInitial: false}, series(css));
+};
